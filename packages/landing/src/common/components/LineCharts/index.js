30e60c1e7ff19728454f6b35ac8b775104cbeada
imstar15 <imstar15@qq.com>
2021-09-26 13:38:31 +0800

    Fix chart

--- packages/landing/src/common/components/LineCharts/index.js
+++ packages/landing/src/common/components/LineCharts/index.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { connect } from 'react-redux';
-import { Line } from '@reactchartjs/react-chart.js'
+import { Line } from '@reactchartjs/react-chart.js';
 import LineChartsStyle from './lineCharts.style';
 import { PolkadotContext } from 'common/contexts/PolkadotContext';
 import backend from 'common/backend';
 import _ from 'lodash';
-import moment from 'moment'
+import moment from 'moment';
 
 const X_NUM = 7;
 
 const LineCharts = (props) => {
   const polkadotContext = useContext(PolkadotContext);
   const [chartData, setChartData] = useState(null);
-  
+
   useEffect(async () => {
     const projectIndex = parseInt(polkadotContext.projectDetail.project_index);
-    const result = await backend.getDatabase().collection('votes').where({
-      projectIndex,
-    }).get();
+    const result = await backend
+      .getDatabase()
+      .collection('votes')
+      .where({
+        projectIndex,
+      })
+      .get();
 
     // Find votes of project
     const votes = result.data;
@@ -29,46 +33,50 @@ const LineCharts = (props) => {
       return;
     }
 
-    // Calculate day section
-    const beginDay = Math.floor(moment.duration(projectVotes[0].timestamp).asDays());
-    const endDay = Math.floor(moment.duration(new Date()).asDays());
+    // get days from 2000-01-01 00:00:00
+    const getDays = (timestamp) =>
+      Math.floor(
+        moment
+          .duration(moment(timestamp) - moment('2000-1-1 00:00:00'))
+          .asDays()
+      );
 
-    let daySection = Math.ceil((endDay - beginDay) / X_NUM);
+    // Calculate day section
+    const beginDay = getDays(projectVotes[0].timestamp);
+    const endDay = getDays(new Date());
+    const dayDistance = endDay - beginDay + 1;
+
+    let daySection = Math.ceil(dayDistance / X_NUM);
     daySection = daySection > 1 ? daySection : 1;
 
-    // Calculate chart datas
+    // Initialize chart data
     const dayDatas = [];
     for (let i = 0; i < X_NUM; i += 1) {
       const day = i * daySection;
-      dayDatas.push({
-        day,
-        amount: (day - 1) > (endDay - beginDay) ? null : 0,
-      });
+      dayDatas.push({ day, amount: day - 1 > dayDistance ? null : 0 });
     }
 
+    // Calculate chart data
     _.each(projectVotes, (vote) => {
-      const day = Math.floor(moment.duration(vote.timestamp).asDays());
+      const day = getDays(vote.timestamp);
       const dayDiff = day - beginDay;
       const index = Math.floor(dayDiff / daySection);
-      console.log('index: ', index);
       dayDatas[index].amount += vote.amount;
     });
 
+    // Accumulate previous votes
     for (let i = 1; i < X_NUM; i += 1) {
-      if ((dayDatas[i].day - 1) > (endDay - beginDay)) {
-        continue;
+      if (dayDatas[i].day > dayDistance) {
+        break;
       }
-      dayDatas[i].amount += dayDatas[i-1].amount;
+      dayDatas[i].amount += dayDatas[i - 1].amount;
     }
 
     // Set chart datas
-    const labels = _.map(dayDatas, (item) => {
-      return moment(projectVotes[0].timestamp).add(item.day, 'day').format('YYYY/M/D');
-    });
-
-    const amounts = _.map(dayDatas, (item) => {
-      return item.amount;
-    });
+    const labels = _.map(dayDatas, (item) =>
+      moment(projectVotes[0].timestamp).add(item.day, 'day').format('YYYY/M/D')
+    );
+    const amounts = _.map(dayDatas, (item) => item.amount);
 
     setChartData({
       labels,
@@ -86,7 +94,13 @@ const LineCharts = (props) => {
 
   return (
     <LineChartsStyle {...props}>
-      {!_.isEmpty(chartData) && (<Line height={370} data={chartData} options={{ maintainAspectRatio: false }}  />)}
+      {!_.isEmpty(chartData) && (
+        <Line
+          height={370}
+          data={chartData}
+          options={{ maintainAspectRatio: false }}
+        />
+      )}
     </LineChartsStyle>
   );
 };
@@ -97,7 +111,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	setAccount: (account) => dispatch(actions.setAccount(account)),
+  setAccount: (account) => dispatch(actions.setAccount(account)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LineCharts);

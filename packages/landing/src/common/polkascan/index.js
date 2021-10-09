722e23e89949e54ecf7b324b620eed0a0f9b6a31
imstar15 <imstar15@qq.com>
2021-10-10 00:42:01 +0800

    Add comments

--- packages/landing/src/common/polkascan/index.js
+++ packages/landing/src/common/polkascan/index.js
@@ -2,54 +2,82 @@ import _ from 'lodash';
 import moment from 'momnet';
 const BASE_URL = 'http://rpc.testnet.oak.tech:8080/api/v1';
 const PAGE_SIZE = 10000;
-const DECIMAL = 10e-11;
+const DECIMALS = 10e9;
 
+/**
+ * Polkascan wrapper
+ */
 class Polkascan {
+	/**
+	 * Get extrinsics
+	 * @param {string} moduleId 
+	 * @param {string} callId 
+	 * @returns 
+	 */
   async getExtrinsics (moduleId, callId) {
-		const url = `${BASE_URL}/extrinsic?filter[signed]=1&filter[module_id]=${moduleId}&filter[call_id]=${callId}&page[size]=${PAGE_SIZE}`;
-		const response = await fetch(url);
-		const responseData = await response.json();
-		let extrinsics = responseData.data;
-		extrinsics = _.filter(extrinsics, { attributes: { success: 1 }})
-		const promises = _.map(extrinsics, async (extrinsic) => {
-			const { attributes } = extrinsic;
-			const { extrinsic_hash } = attributes;
-			return this.getExtrinsicDetail(extrinsic_hash);
-		});
-		const promiseResults = await Promise.allSettled(promises);
-		_.each(promiseResults, (promiseResult, index) => {
-			if (promiseResult.status === 'fulfilled') {
-				const { attributes } = extrinsics[index];
-				const { params, datetime } =  promiseResult.value;
-				attributes.params = params;
-				attributes.datetime = datetime;
-			}
-		});
-		return extrinsics;
+		try {
+			const url = `${BASE_URL}/extrinsic?filter[signed]=1&filter[module_id]=${moduleId}&filter[call_id]=${callId}&page[size]=${PAGE_SIZE}`;
+			const response = await fetch(url);
+			const responseData = await response.json();
+			let extrinsics = responseData.data;
+			extrinsics = _.filter(extrinsics, { attributes: { success: 1 }})
+			const promises = _.map(extrinsics, async (extrinsic) => {
+				const { attributes } = extrinsic;
+				const { extrinsic_hash } = attributes;
+				return this.getExtrinsicDetail(extrinsic_hash);
+			});
+			const promiseResults = await Promise.allSettled(promises);
+			_.each(promiseResults, (promiseResult, index) => {
+				if (promiseResult.status === 'fulfilled') {
+					const { attributes } = extrinsics[index];
+					const { params, datetime } =  promiseResult.value;
+					attributes.params = params;
+					attributes.datetime = datetime;
+				}
+			});
+			return extrinsics;
+		} catch (err) {
+			console.log(err);
+			return [];
+		}
   }
 
-	async getExtrinsicDetail (hash) {
-		const url = `${BASE_URL}/extrinsic/0x${hash}`;
+	/**
+	 * Get extrinsic detail
+	 * @param {string} extrinsicHash 
+	 * @returns 
+	 */
+	async getExtrinsicDetail (extrinsicHash) {
+		const url = `${BASE_URL}/extrinsic/0x${extrinsicHash}`;
 		const response = await fetch(url);
 		const responseData = await response.json();
 		const { data: { attributes }}  = responseData;
 		return attributes;
 	}
 
+	/**
+	 * Get contributions by project index
+	 * @param {number} projectIndex 
+	 * @returns 
+	 */
 	async getContributionsByProjectIndex(projectIndex) {
 		let extrinsics = await this.getExtrinsics('QuadraticFunding', 'contribute');
 		extrinsics = _.filter(extrinsics, (extrinsic) => {
 			const { attributes } = extrinsic;
 			const { params } = attributes;
+			if (!params) {
+				return false;
+			}
 			const projectIndexParam = _.find(params, { name: 'project_index' });
-			return projectIndexParam.value === projectIndex;
+			return projectIndexParam && projectIndexParam.value === projectIndex;
 		});
-		const votes = _.map(extrinsics, (extrinsic) => {
+		const contributions = _.map(extrinsics, (extrinsic) => {
 			const { attributes: { address, params, datetime } } =  extrinsic;
 			const valueParam = _.find(params, { name: 'value' });
-			return { address, amount: valueParam.value * DECIMAL, timestamp: moment(datetime).valueOf() };
+			const amount = valueParam ? valueParam.value / DECIMALS : 0;
+			return { address, amount, timestamp: moment(datetime).valueOf() };
 		});
-		return votes;
+		return contributions;
 	}
 }
 
